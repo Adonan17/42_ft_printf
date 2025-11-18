39e0c826424ad6f24fd84c64011a9e84a318b966
Adonan17 <adnaneouassari@gmail.com>
2025-11-18 16:10:42 +0100

    finishing header file

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,22 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 16:17:01 by aouassar          #+#    #+#             */
-/*   Updated: 2025/11/18 15:57:18 by aouassar         ###   ########.fr       */
+/*   Updated: 2025/11/18 16:05:41 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_print_char(int c);
+int	ft_print_str(char *str);
+int	ft_print_nbr(int n);
+int	ft_print_uint(unsigned int n);
+int	ft_print_hex(unsigned int n, char format);
+int	ft_print_ptr(unsigned long long ptr);
+
+#endif
