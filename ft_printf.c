24c20d5b58bdb6f227ffbdacb365b8265f78d931
Adonan17 <adnaneouassari@gmail.com>
2025-11-20 10:21:55 +0100

    updating the ft_print function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,40 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 16:17:13 by aouassar          #+#    #+#             */
-/*   Updated: 2025/11/17 16:17:14 by aouassar         ###   ########.fr       */
+/*   Updated: 2025/11/20 10:00:44 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+static int	ft_formats(va_list args, char specifier)
+{
+	
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	size_t	i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		else
+		{
+			count += ft_formats(args, format[i + 1]);
+			i++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
