b446b0711092e90e5969d062d5638500f0935e5f
Adonan17 <adnaneouassari@gmail.com>
2025-11-22 16:25:01 +0100

    adding helper for uint

--- ft_print_uint.c
+++ ft_print_uint.c
@@ -6,7 +6,32 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 16:17:16 by aouassar          #+#    #+#             */
-/*   Updated: 2025/11/17 16:17:17 by aouassar         ###   ########.fr       */
+/*   Updated: 2025/11/22 16:24:25 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+static int	ft_putunbr(unsigned long nb)
+{
+	char	c;
+	int		count;
+
+	count = 0;
+	if (nb >= 10)
+		count += ft_putunbr(nb / 10);
+	c = '0' + (nb % 10);
+	write(1, &c, 1);
+	return(count + 1);
+}
+
+int	ft_print_uint(unsigned int n)
+{
+	long	nb;
+	int		count;
+	
+	count = 0;
+	nb = n;
+	count += ft_putunbr(nb);
+	return (count);
+}
